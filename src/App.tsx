49a3ddc5932f1c34cd492255964e4b96ac09d987
Sergei <sergeidubovskiy1990@gmail.com>
2023-01-07 14:29:14 +0300

    finish less1

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     const task2 = [
         {id: 1, title: "Hello World", isDone: true},
         {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
+        {id: 3, title: "Yo", isDone: false},
         {id: 4, title: "Yo", isDone: false}
     ]
     return (
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -20,16 +20,11 @@ export const Todolist = (props: TodolistPropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                {props.tasks.map((el )=>{
-                    return(
+                {props.tasks.map((el) => {
+                    return (
                         <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
                     )
                 })}
-                <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
